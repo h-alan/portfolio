1cbf36400589c0ad3fd1005b44f9cf4c779e9b62
h-alan <battlebearboss@gmail.com>
2023-07-27 21:07:25 -0400

    Finish template for website, added darkmode

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,112 @@
+'use client'
+
 import { BsFillMoonStarsFill } from 'react-icons/bs'
-import { AiFillTwitterCircle, AiFillLinkedin, AiFillYoutube } from 'react-icons/ai'
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
 import Image from 'next/image'
 import deved from '../public/dev-ed-wave.png'
+import design from '../public/design.png'
+import code from '../public/code.png'
+import consulting from '../public/consulting.png'
+import web1 from '../public/web1.png'
+import web2 from '../public/web2.png'
+import web3 from '../public/web3.png'
+import web4 from '../public/web4.png'
+import web5 from '../public/web5.png'
+import web6 from '../public/web6.png'
+import { useState } from 'react'
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <main className='bg-white px-10 '>
-      <section className='min-h-screen'>
-        <nav className='py-10 mb-12 flex justify-between'>
-          <h1 className='text-xl font-burtons'>h-alan</h1>
-          <ul className='flex items-center'>
-            <li><BsFillMoonStarsFill className='cursor-pointer text-2xl' /></li>
-            <li><a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="#">Resume</a></li>
-          </ul>
-        </nav>
-        <div className='text-center p-10'>
-          <h2 className='text-5xl py-2 text-teal-600 font-medium'>Alan Han</h2>
-          <h3 className='text-2xl py-2'>Student and Researcher</h3>
-          <p className='text-md py-5 leading-8 text-gray-800'>Looking to learn more about Software Engineering! My recent projects are listed down below.</p>
-        </div>
-        <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
-          <AiFillTwitterCircle />
-          <AiFillLinkedin />
-          <AiFillYoutube />
-        </div>
-        <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden'>
-          <Image src={deved} layout='fill' objectFit='cover' alt='' />
-        </div>
-      </section>
-    </main>
+    <div className={darkMode ? 'dark' : ''}>
+      <main className='bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900'>
+        {/* Introduction and Description */}
+        <section className='min-h-screen'>
+          <nav className='py-10 mb-12 flex justify-between'>
+            <h1 className='text-xl font-burtons dark:text-white'>h-alan</h1>
+            <ul className='flex items-center'>
+              <li><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='cursor-pointer text-2xl dark:text-white' /></li>
+              <li><a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="#">Resume</a></li>
+            </ul>
+          </nav>
+          <div className='text-center p-10'>
+            <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl dark:text-white'>Alan Han</h2>
+            <h3 className='text-2xl py-2 md:text-3xl dark:text-white'>Student and Researcher</h3>
+            <p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-white'>Looking to learn more about Software Engineering! My recent projects are listed down below.</p>
+          </div>
+          <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-white'>
+            <a target="_blank" rel="noopener noreferrer" href='https://github.com/h-alan'><AiFillGithub /></a>
+            <a target="_blank" rel="noopener noreferrer" href='https://www.linkedin.com/in/alan-han-2b7b0a23a/'><AiFillLinkedin /></a>
+          </div>
+          <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96'>
+            <Image src={deved} layout='fill' objectFit='cover' alt='' />
+          </div>
+        </section>
+
+        {/* Cards and Skills */}
+        <section>
+          <div>
+            <h3 className='text-3xl py-1 dark:text-white'>Services I offer</h3>
+            <p className='text-md py-2 leading-8 text-gray-800 dark:text-white'>Object-Oriented Programming & Data Structures,
+              <span className='text-teal-500'> Data Structures and Algorithms</span>, Functional Programming, Linear Algebra,
+              <span className='text-teal-500'> Foundations of Artificial Intelligence</span>, Differential Equations</p>
+            <p className='text-md py-2 leading-8 text-gray-800 dark:text-white'>
+              Currently conducting research in Software Engineering related to testing and runtime verification.
+            </p>
+          </div>
+          <div className='lg:flex gap-10'>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+              <Image src={design} width={100} height={100} alt='' />
+              <h3 className='text-large font-medium pt-8 pb-2 dark:text-white'>Beautiful Designs</h3>
+              <p className='py-2 dark:text-white'>Creating elegant designs suited for your needs following our core design theory</p>
+              <h4 className='py-4 text-teal-600'>Design tools I use</h4>
+              <p className='text-gray-800 py-1 dark:text-white'>Photoshop</p>
+              <p className='text-gray-800 py-1 dark:text-white'>Illustrator</p>
+              <p className='text-gray-800 py-1 dark:text-white'>Figma</p>
+            </div>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+              <Image src={code} width={100} height={100} alt='' />
+              <h3 className='text-large font-medium pt-8 pb-2 dark:text-white'>Beautiful Designs</h3>
+              <p className='py-2 dark:text-white'>Creating elegant designs suited for your needs following our core design theory</p>
+              <h4 className='py-4 text-teal-600'>Design tools I use</h4>
+              <p className='text-gray-800 py-1 dark:text-white'>Photoshop</p>
+              <p className='text-gray-800 py-1 dark:text-white'>Illustrator</p>
+              <p className='text-gray-800 py-1 dark:text-white'>Figma</p>
+            </div>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+              <Image src={consulting} width={100} height={100} alt='' />
+              <h3 className='text-large font-medium pt-8 pb-2 dark:text-white'>Beautiful Designs</h3>
+              <p className='py-2 dark:text-white'>Creating elegant designs suited for your needs following our core design theory</p>
+              <h4 className='py-4 text-teal-600'>Design tools I use</h4>
+              <p className='text-gray-800 py-1 dark:text-white'>Photoshop</p>
+              <p className='text-gray-800 py-1 dark:text-white'>Illustrator</p>
+              <p className='text-gray-800 py-1 dark:text-white'>Figma</p>
+            </div>
+          </div>
+        </section>
+
+        {/* Project Section */}
+        <section>
+          <div>
+            <h3 className='text-3xl py-1 dark:text-white'>Projects</h3>
+            <p className='text-md py-2 leading-8 text-gray-800 dark:text-white'>Object-Oriented Programming & Data Structures,
+              <span className='text-teal-500'> Data Structures and Algorithms</span>, Functional Programming, Linear Algebra,
+              <span className='text-teal-500'> Foundations of Artificial Intelligence</span>, Differential Equations</p>
+            <p className='text-md py-2 leading-8 text-gray-800 dark:text-white'>
+              Currently conducting research in Software Engineering related to testing and runtime verification.
+            </p>
+          </div>
+          <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
+            <div className='basis-1/3 flex-1'><Image className='rounded-lg object-cover' layout='responsive' src={web1} alt='' /></div>
+            <div className='basis-1/3 flex-1'><Image className='rounded-lg object-cover' layout='responsive' src={web2} alt='' /></div>
+            <div className='basis-1/3 flex-1'><Image className='rounded-lg object-cover' layout='responsive' src={web3} alt='' /></div>
+            <div className='basis-1/3 flex-1'><Image className='rounded-lg object-cover' layout='responsive' src={web4} alt='' /></div>
+            <div className='basis-1/3 flex-1'><Image className='rounded-lg object-cover' layout='responsive' src={web5} alt='' /></div>
+            <div className='basis-1/3 flex-1'><Image className='rounded-lg object-cover' layout='responsive' src={web6} alt='' /></div>
+          </div>
+        </section>
+      </main>
+    </div>
   )
 }
